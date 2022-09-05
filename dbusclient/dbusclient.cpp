1f9451905dc21b74ceeea929609f119075d77e22
tkhshmsy <tkhshmsy@gmail.com>
2022-09-05 16:29:16 +0900

    use QMetaObject's methods

--- dbusclient/dbusclient.cpp
+++ dbusclient/dbusclient.cpp
@@ -7,7 +7,6 @@
 // ====
 // in DBusServer side(dbusserver.cpp),
 // count() is not created as method, but as property.
-// we should use 'org.freedesktop.DBus.Properties.Get'
 // ====
 #define USE_DBUS_PROPERTY
 
@@ -18,9 +17,6 @@ public:
     ~Private();
     QDBusConnection bus = QDBusConnection::sessionBus();
     QDBusInterface *iface = Q_NULLPTR;
-#ifdef USE_DBUS_PROPERTY
-    QDBusInterface *ifaceProperty = Q_NULLPTR;
-#endif
 };
 
 DBusClient::Private::Private()
@@ -33,16 +29,6 @@ DBusClient::Private::Private()
         qWarning() << "failed to make DBusInterface";
         exit(1);
     }
-#ifdef USE_DBUS_PROPERTY
-    ifaceProperty =  new QDBusInterface("local.myhost",           // from D-Bus Service
-                                        "/QtDBusDemo/DBusServer", // from D-Bus Path
-                                        "org.freedesktop.DBus.Properties",
-                                        QDBusConnection::sessionBus());
-    if (!ifaceProperty) {
-        qWarning() << "failed to make DBusInterface for property";
-        exit(1);
-    }
-#endif
 }
 
 DBusClient::Private::~Private()
@@ -50,11 +36,6 @@ DBusClient::Private::~Private()
     if (iface) {
         delete iface;
     }
-#ifdef USE_DBUS_PROPERTY
-    if (ifaceProperty) {
-        delete ifaceProperty;
-    }
-#endif
 }
 
 DBusClient::DBusClient(QObject *parent)
@@ -62,12 +43,12 @@ DBusClient::DBusClient(QObject *parent)
     , d{new Private}
 {
     // connect SIGNAL(DBusServer::countChanged)
-    bool flag = d->bus.connect(d->iface->service(),
-                               d->iface->path(),
-                               d->iface->interface(),
-                               QStringLiteral("countChanged"),
-                               this,
-                               SLOT(countChanged(int)));
+    bool flag = d->iface->connection().connect(d->iface->service(),
+                                               d->iface->path(),
+                                               d->iface->interface(),
+                                               QStringLiteral("countChanged"),
+                                               this,
+                                               SLOT(countChanged(int)));
     if (!flag) {
         qDebug() << "failed to connect DBus signal";
     }
@@ -89,13 +70,8 @@ int DBusClient::count() const
     qDebug() << __func__ ; // for debug
 
 #ifdef USE_DBUS_PROPERTY
-    // call count() via org.freedesktop.DBus.Properties.Get
-    QDBusReply<QVariant> countReply = d->ifaceProperty->call("Get", "api.dbusserver", "count");
-    if (!countReply.isValid()) {
-        qDebug() << "invalid reply from count() with DBusProperty";
-        return 0;
-    }
-    int count = countReply.value().toInt();
+    QVariant reply = d->iface->property("count");
+    int count = reply.toInt();
 #else
     // call("count") will be failed !!,
     // because of lack of Q_INVOKABLE on count() in dbusserver.h
@@ -112,21 +88,20 @@ int DBusClient::count() const
 void DBusClient::reset()
 {
     qDebug() << __func__ ; // for debug
-
-    // call method reset()
-    QDBusReply<void> resetReply = d->iface->call("reset");
-    if (!resetReply.isValid()) {
-        qDebug() << "invalid reply from reset()";
-    }
+    QMetaObject::invokeMethod(d->iface, "reset");
 }
 
 void DBusClient::setCount(int newCount)
 {
     qDebug() << __func__ << newCount; // for debug
 
+#ifdef USE_DBUS_PROPERTY
+    d->iface->setProperty("count", QVariant(newCount));
+#else
     // call method setCount()
     QDBusReply<void> setCountReply = d->iface->call("setCount", newCount);
     if (!setCountReply.isValid()) {
         qDebug() << "invalid reply from setCount()";
     }
+#endif
 }
